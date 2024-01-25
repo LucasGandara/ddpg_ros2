72d8c92a4cd3a6cd1738e4ab29b423d63c02657f
Lucas Gandara <lucas17-12@hotmail.com>
2024-01-25 18:39:39 -0500

    Goal box respawn working.

--- ddpg_ros2/respawn_goal.py
+++ ddpg_ros2/respawn_goal.py
@@ -2,27 +2,31 @@
 # -*- utf-8
 
 # Example: https://github.com/ROBOTIS-GIT/turtlebot3_machine_learning/blob/master/turtlebot3_dqn/src/turtlebot3_dqn/respawnGoal.py
+
 import os
 from geometry_msgs.msg import Pose
 from rclpy.node import Node
 import rclpy
 from gazebo_msgs.srv import SpawnEntity, GetModelList, DeleteEntity
+from rclpy.logging import LoggingSeverity
+import sys
+from rclpy.executors import ExternalShutdownException
 
 
 class Respawn:
     def __init__(self, node: Node) -> None:
         self.node = node
         file_path = os.path.dirname(os.path.realpath(__file__))
-        print(file_path)
+
         self.model_path = file_path.replace(
             "/ddpg_ros2/ddpg_ros2", "/ddpg_ros2/models/goal.sdf"
         )
+
+        self.node.get_logger().debug(f"model path: {self.model_path}")
         self.file = open(self.model_path, "r")
         self.model_description = self.file.read()
 
-        self.model_subscriber = node.create_subscription(
-            GetModelList, "/get_model_list", self.model_subscriber_callback, 10
-        )
+        self.model_service_client = node.create_client(GetModelList, "/get_model_list")
 
         # Delete entity service
         self.delete_entity_client = node.create_client(DeleteEntity, "/delete_entity")
@@ -48,25 +52,60 @@ class Respawn:
                 self.check_model = True
                 break
 
-    def check_model(self, model):
+    def check_model_call(self, model: str = None):
+        self.node.get_logger().debug(
+            "List model service available, waiting until is ready..."
+        )
+        while not self.model_service_client.wait_for_service(timeout_sec=1.0):
+            self.node.get_logger().info("service not available, waiting again...")
+
+        self.node.get_logger().debug("List model service available, calling service...")
+        model_list_request = self.model_service_client.call_async(
+            GetModelList.Request()
+        )
+        rclpy.spin_until_future_complete(self.node, model_list_request, timeout_sec=3.0)
+
+        self.node.get_logger().debug("List model service response received")
         self.check_model = False
-        for i in range(len(model.name)):
-            if model.name[i] == "goal":
-                self.check_model = True
+        for i in range(len(model_list_request.result().model_names)):
+            if model:
+                if model_list_request.result().model_names[i] == model:
+                    self.check_model = True
+                    self.node.get_logger().debug("List model service: Model found")
+            else:
+                if model_list_request.result().model_names[i] == self.entity_name:
+                    self.check_model = True
+                    self.node.get_logger().debug("List model service: Model found")
 
     def respawn_entity(self):
-        while True:
-            if not self.check_model:
-                while not self.spawn_entity_client.wait_for_service(timeout_sec=2.0):
-                    self.node.get_logger().info(
-                        "service not available, waiting again..."
-                    )
-                self.spawn_entity_client.call_async(
-                    SpawnEntity.Request(self.goal_position, "goal", "robot_description")
+        self.node.get_logger().debug(
+            "Respawn entity service available, waiting until is ready..."
+        )
+
+        print(self.check_model)
+
+        if not self.check_model:
+            while not self.spawn_entity_client.wait_for_service(timeout_sec=2.0):
+                self.node.get_logger().info(
+                    "Respawn entity service not available, waiting again..."
                 )
-                break
-            else:
-                pass
+
+            self.node.get_logger().debug("Respawn entity calling service...")
+
+            request = SpawnEntity.Request()
+            request.name = self.entity_name
+            request.xml = self.model_description
+            request.robot_namespace = ""
+            request.initial_pose = self.goal_position
+            spawn_entity_request = self.spawn_entity_client.call_async(request)
+
+            response = rclpy.spin_until_future_complete(
+                self.node, spawn_entity_request, timeout_sec=3.0
+            )
+
+            self.node.get_logger().debug(
+                f"Respawn entity service response received: {response}"
+            )
 
     def delete_model(self):
         while True:
@@ -85,5 +124,24 @@ class Respawn:
 
 if __name__ == "__main__":
     rclpy.init()
+
     client = rclpy.create_node("respawn_goal")
-    Respawn(client)
+    client.get_logger().set_level(LoggingSeverity.DEBUG)
+
+    respawn = Respawn(client)
+
+    respawn.check_model_call()  # Should return false
+
+    respawn.respawn_entity()
+
+    respawn.check_model_call()  # Should return true
+    try:
+        rclpy.spin(client)
+    except KeyboardInterrupt:
+        pass
+    except ExternalShutdownException:
+        sys.exit(1)
+    finally:
+        print("\nDestroying node")
+        rclpy.try_shutdown()
+        client.destroy_node()
--- ddpg_ros2/tb3_gym_env.py
+++ ddpg_ros2/tb3_gym_env.py
@@ -1,55 +1,79 @@
 #!/usr/bin/env python3
 # turtlebot model: ~/ros2_ws/install/turtlebot3_gazebo/share/turtlebot3_gazebo/launch/spawn_turtlebot3.launch.py
 
-import rclpy
+import gymnasium as gym
+import numpy as np
 from rclpy.node import Node
-from std_msgs.msg import String
-import time
+from sensor_msgs.msg import LaserScan
+from rclpy.executors import ExternalShutdownException
 from respawn_goal import Respawn
+import sys
 
-from rcl_interfaces.srv import ListParameters
 
+class ENV(gym.Env):
+    metadata = {"render_modes": ["gazebo"]}
 
-class DDPG_NODE(Node):
-    def __init__(self):
-        super().__init__("ddpg")
-        self.publisher_ = self.create_publisher(String, "topic", 10)
+    def __init__(self, node: Node) -> None:
+        # ROS related stuff
+        self.node = node
+        self.laser_scan = np.zeros(24)
+        node.create_subscription(LaserScan, "/scan", self.laser_callback, 10)
 
-        # Wait for services
-        self.respawn_service = Respawn(self)
+        # Goal object
+        self.respawn_goal = Respawn(node)
 
-        tb3_gazebo_service = self.create_client(
-            ListParameters, "/robot_state_publisher/list_parameters"
+        # Env related stuff
+        self.action_space = gym.spaces.Box(
+            low=np.array([0, -2.0]), high=np.array([0, 2.0]), dtype=np.float32
         )
-        while not tb3_gazebo_service.wait_for_service(timeout_sec=2.0):
-            self.get_logger().info("service not available, waiting again...")
-        time.sleep(2)
 
-        timer_period = 0.5  # seconds
-        self.timer = self.create_timer(timer_period, self.timer_callback)
-        self.i = 0
+        self.observation_space = gym.spaces.Dict(
+            {
+                "laser": gym.spaces.Box(low=0, high=3.5, shape=(24,)),
+            }
+        )
 
-    def timer_callback(self):
-        msg = String()
-        msg.data = "Hello World: %d" % self.i
-        self.publisher_.publish(msg)
-        self.get_logger().info('Publishing: "%s"' % msg.data)
-        self.i += 1
+    def laser_callback(self, msg: LaserScan):
+        self.laser_scan = np.array(msg.ranges)
 
+    def _get_obs(self):
+        distance_to_goal = 1
+        angle_to_goal = 0
+        return dict(
+            {
+                "laser": self.laser_scan,
+                "distance_to_goal": distance_to_goal,
+                "angle_to_goal": angle_to_goal,
+            }
+        )
 
-def main(args=None):
-    rclpy.init(args=args)
+    def _get_info(self):
+        position = 0
+        return dict({"position": position})
 
-    minimal_publisher = DDPG_NODE()
+    def reset(self, seed=None, options=None):
+        super().reset(seed=seed)
 
-    rclpy.spin(minimal_publisher)
+        observation = self._get_obs()
+        info = self._get_info()
 
-    # Destroy the node explicitly
-    # (optional - otherwise it will be done automatically
-    # when the garbage collector destroys the node object)
-    minimal_publisher.destroy_node()
-    rclpy.shutdown()
+        return observation, info
 
 
 if __name__ == "__main__":
-    main()
+    import rclpy
+
+    rclpy.init()
+    node = rclpy.create_node("tb3_gym_env")
+    env = ENV(node)
+
+    try:
+        rclpy.spin(node)
+    except KeyboardInterrupt:
+        pass
+    except ExternalShutdownException:
+        sys.exit(1)
+    finally:
+        print("\nDestroying node")
+        rclpy.try_shutdown()
+        node.destroy_node()
